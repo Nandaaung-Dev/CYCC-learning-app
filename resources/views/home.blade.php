484a6a207846cecd77803bf79149f91eb4c5ab52
Nanda <nandaaung1689@gmail.com>
2024-07-03 23:29:07 +0630

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,9 +48,9 @@
         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative p-4 w-full max-w-md max-h-full">
             <!-- Modal content -->
-            <div class="relative rounded-lg bg-[#f8d5ec] shadow bg-image">
+            <div class="relative rounded-lg shadow hello">
                 <!-- Modal header -->
-                <div class="flex justify-end">
+                <div class="flex justify-end mb-16">
                     <button type="button"
                         class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                         data-modal-hide="authentication-modal">
@@ -62,9 +62,9 @@
                         <span class="sr-only">Close modal</span>
                     </button>
                 </div>
-                <div class="flex flex-col items-center pt-[96px]">
+                <div class="flex flex-col items-center pt-4">
                     <img src="{{ asset('images/footer-2.png') }}" alt="" srcset="" class=" w-[212px]">
-                    <h1 class=" py-[20px] font-bold text-2xl">Welcome to <span class=" text-[#FB56C2] ">CYCC</span></h1>
+                    <h1 class=" py-[10px] font-bold text-2xl">Welcome to <span class=" text-[#FB56C2] ">CYCC</span></h1>
 
                 </div>
                 <!-- Modal body -->
@@ -95,6 +95,17 @@
 
 </html>
 <style>
+    .hello {
+        background-image: url(images/login-1.png);
+        background-size: 500px 550px;
+        /* Width and height of the background image */
+        /* Adjust width, maintain aspect ratio */
+        background-repeat: no-repeat;
+        /* Prevent image from repeating */
+        background-position: center;
+        object-fit: cover;
+    }
+
     .toast-error {
         background-color: rgb(240, 57, 57);
     }
