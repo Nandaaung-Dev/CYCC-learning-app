85bfb579479863f17f17bcc564ecfa9c9ca02bc6
Nanda <nandaaung1689@gmail.com>
2024-07-06 20:01:17 +0630

    update

--- database/migrations/2024_07_03_132700_create_courses_table.php
+++ database/migrations/2024_07_03_132700_create_courses_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longText('description');
+            $table->string('video');
             $table->string('rating');
             $table->string('duration');
             $table->string('reviews');
